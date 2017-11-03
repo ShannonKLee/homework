885bafaa313bdc185696785b36bc34b3704e21da
Shannon Lee <shannon.k.lee@hotmail.com>
2017-11-03 12:05:11 -0400

    Created calculated_marked_up_price docstring with example

---
+++ price_packager.py
@@ -0,0 +1,27 @@
+def calculate_marked_up_price(base_price, num_people, type):
+	""" Calculate the marked up value of an item.
+
+	Price packager will first calculate the flat mark up rate of 5%
+	and then the persons markup of 1.2% per person and the type markup 
+	respectively, if they exist.
+
+	Parameters
+	----------
+	base_price : float
+		The initial price of the item
+	num_people : int
+		The number of people that worked on the item
+	type : str
+		The category the item falls under. Each type has its own markup.
+
+	Returns
+	-------
+	float
+		The final price of the item after mark up calculations
+
+	Examples
+	--------
+	>>> calculate_marked_up_price(1299.99, 3, 'food')
+	1591.58
+	"""
+	
